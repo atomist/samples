5964b9dbcf3fd6900e51a917d3b5130a59c1b477
Christian Dupuis <cd@atomist.com>
2019-06-04 21:35:32 +0200

    Polish dotnetcore example

--- lib/sdm/dotnetCore.ts
+++ lib/sdm/dotnetCore.ts
@@ -11,11 +11,11 @@ import {
 } from "@atomist/sdm-core";
 import {
     dotnetCoreBuilder,
+    DotnetCoreProjectFileCodeTransform,
     DotnetCoreProjectVersioner,
     DotnetCoreVersionProjectListener,
     getDockerfile,
 } from "@atomist/sdm-pack-analysis-dotnet";
-import { DotnetCoreProjectFileCodeTransform } from "@atomist/sdm-pack-analysis-dotnet/lib/tranform/dotnetCoreTransforms";
 import { Build } from "@atomist/sdm-pack-build";
 import {
     DockerBuild,
@@ -30,7 +30,11 @@ import { UpdateReadmeTitle } from "../transform/updateReadmeTitle";
  * @tag sdm,generator,dotnet-core
  * @instructions <p>Now that the SDM is up and running, create a new .NET Core
  *               project by running '@atomist create dotnet-core project' and
- *               observe how the SDM will build and dockerize the new project.</p>
+ *               observe how the SDM will build and dockerize the new project.
+ *
+ *               The docker build and run goals require a locally accessible
+ *               docker daemon. Please make sure to configure your terminal for
+ *               docker access.</p>
  */
 
 // atomist:code-snippet:start=dotnetGenerator
@@ -57,9 +61,11 @@ export const configuration = configure(async sdm => {
     // Register the generator with the SDM
     sdm.addGeneratorCommand(DotnetCoreGenerator);
 
+    // Version goal calculates a timestamped version for the build goal
     const versionGoal = new Version()
         .withVersioner(DotnetCoreProjectVersioner);
 
+    // Build goal that runs "dotnet build"
     const buildGoal = new Build(
         { displayName: "dotnet build" })
         .with({
@@ -67,15 +73,17 @@ export const configuration = configure(async sdm => {
             builder: dotnetCoreBuilder(),
         }).withProjectListener(DotnetCoreVersionProjectListener);
 
+    // Docker build to wrap the app into a container image
     const dockerBuildGoal = new DockerBuild()
         .with({
-            dockerfileFinder: getDockerfile,
-            push: false,
+            dockerfileFinder: getDockerfile, // where to find the Dockerfile
+            push: false, // skip pushing the image to a remote repository; can be enabled by providing credentials
             dockerImageNameCreator: async (p, sdmGoal) => [
                 { registry: p.id.owner, name: p.id.repo, tags: [`${sdmGoal.branch}-${sdmGoal.sha.slice(0, 7)}`, "latest"] },
             ],
         });
 
+    // Docker run goal to start the application in a container
     const dockerRunGoal = goal(
         { displayName: "docker run" },
         async gi => {
@@ -84,6 +92,7 @@ export const configuration = configure(async sdm => {
             return spawnLog("docker", ["run", "-d", "-p", "8080:8080", image], { log: progressLog });
         });
 
+    // This SDM has two PushRules: build and docker
     return {
         build: {
             test: hasFileWithExtension("csproj"),
